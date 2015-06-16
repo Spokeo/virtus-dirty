b8500e61c15a506fef874fe4be6d0dd1876068bd
Eric Liang <eric@spokeo.com>
2015-06-16 14:39:39 -0700

    Remove circular dependency. Bump version to 0.0.6

--- lib/virtus/dirty/version.rb
+++ lib/virtus/dirty/version.rb
@@ -1,5 +1,5 @@
 module Virtus
   module Dirty
-    VERSION = "0.0.5"
+    VERSION = "0.0.6"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-unless defined? Virtus::Dirty
-  $:.unshift File.expand_path('../../lib', __FILE__)
-  require 'virtus/dirty'
-end
+require 'virtus/dirty'
 
 Dir[File.expand_path("**/shared/**/*.rb", File.dirname(__FILE__))].each {|f| require f}
